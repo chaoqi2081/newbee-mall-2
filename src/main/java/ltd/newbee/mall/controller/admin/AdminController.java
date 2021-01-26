f1557c89ac99cf209ef7f522c133d8d9e0ceaa5b
chaoqi <229180689@qq.com>
2021-01-26 14:39:13 +0800

    修正空字符串判定

--- src/main/java/ltd/newbee/mall/controller/admin/AdminController.java
+++ src/main/java/ltd/newbee/mall/controller/admin/AdminController.java
@@ -3,8 +3,11 @@ package ltd.newbee.mall.controller.admin;
 import ltd.newbee.mall.entity.AdminUser;
 import ltd.newbee.mall.service.AdminUserService;
 import org.springframework.stereotype.Controller;
-import org.springframework.util.StringUtils;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.util.ObjectUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpSession;
@@ -27,16 +30,16 @@ public class AdminController {
                         @RequestParam("password") String password,
                         @RequestParam("verifyCode") String verifyCode,
                         HttpSession session) {
-        if (StringUtils.isEmpty(verifyCode)) {
+        if (ObjectUtils.isEmpty(verifyCode)) {
             session.setAttribute("errorMsg", "验证码不能为空");
             return "admin/login";
         }
-        if (StringUtils.isEmpty(userName) || StringUtils.isEmpty(password)) {
+        if (ObjectUtils.isEmpty(userName) || ObjectUtils.isEmpty(password)) {
             session.setAttribute("errorMsg", "用户名或密码不能为空");
             return "admin/login";
         }
         String kaptchaCode = session.getAttribute("verifyCode") + "";
-        if (StringUtils.isEmpty(kaptchaCode) || !verifyCode.equals(kaptchaCode)) {
+        if (ObjectUtils.isEmpty(kaptchaCode) || !verifyCode.equals(kaptchaCode)) {
             session.setAttribute("errorMsg", "验证码错误");
             return "admin/login";
         }
